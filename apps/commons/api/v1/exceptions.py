47821628f6dd9381def4e2d4e8f1f74c963da2b3
Ranierelp <ranierepereira21@outlook.com>
2026-05-13 15:08:25 -0300

    fix: remove exception handler com chamada externa ao Google Translate

--- apps/commons/api/v1/exceptions.py
+++ apps/commons/api/v1/exceptions.py
@@ -9,7 +9,6 @@ converting the Django
 """
 import logging
 
-from deep_translator import GoogleTranslator
 from rest_framework import status
 from rest_framework.exceptions import ValidationError as DRFValidationError
 from rest_framework.views import Response, exception_handler as drf_exception_handler
@@ -80,62 +79,3 @@ def _handle_not_found_error(exc, context, response):
     else:
         response = _handle_generic_error(exc, context, response)
     return response
-
-
-def protuguese_exception_handler(exc, context):
-    """Handle Django ValidationError as an accepted exception
-    Must be set in settings:
-    REST_FRAMEWORK = {
-    ...     # ...
-    ...     'EXCEPTION_HANDLER': 'apps.commons.api.v1.exceptions.protuguese_exception_handler',
-    ...     # ...
-    ... }
-    For the parameters, see ``exception_handler``.
-    """
-    if isinstance(exc, Exception):
-        if hasattr(exc, "message_dict"):
-            if type(exc.message_dict) == dict:
-                errors = ""
-                for value in exc.message_dict.values():
-                    if type(value) == list:
-                        value = "".join(value)
-                    errors += GoogleTranslator(source="en", target="pt").translate(
-                        value
-                    )
-                exc = DRFValidationError(detail={"erro": errors})
-        elif hasattr(exc, "message"):
-            exc = DRFValidationError(
-                detail={
-                    "erro": GoogleTranslator(source="en", target="pt").translate(
-                        exc.message
-                    )
-                }
-            )
-        else:
-            errors = ""
-            if type(exc.args) == tuple:
-                try:
-                    for value in exc.args:
-                        value = "".join(
-                            [e.title() for e in list(dict(value).values())[0]]
-                        )
-                        errors += GoogleTranslator(source="en", target="pt").translate(
-                            f"{value} ", dest="pt"
-                        )
-                except ValueError:
-                    for value in exc.args:
-                        errors += GoogleTranslator(source="en", target="pt").translate(
-                            f"{value.title()} ", dest="pt"
-                        )
-            else:
-                for data in [dict(dt) for dt in exc.args]:
-                    for _key, values in data.items():
-                        for value in values:
-                            errors += GoogleTranslator(
-                                source="en", target="pt"
-                            ).translate(f"{value.title()} ", dest="pt")
-            exc = DRFValidationError(detail={"erro": errors})
-
-        logging.error(f"Original error detail and callstack: {exc}")
-
-    return drf_exception_handler(exc, context)
